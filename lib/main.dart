134f58a0db24cfce045248d4ddc3640f92369787
anshulkataria4 <anshulkataria4@gmail.com>
2019-12-28 22:46:57 +0530

    adding containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: 100,
+                color: Colors.red,
+                child: Text('Hello'),
+              ),
+              Container(
+                width: 55,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.yellow,
+                    child: Text('Hello'),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.green,
+                    child: Text('Hello'),
+                  ),
+                ],
+              ),
+              Container(
+                width: 29,
+              ),
+              Container(
+                height: double.infinity,
+                width: 100,
+                margin: EdgeInsets.only(left: 30.0, right: 0),
+                color: Colors.blue,
+                child: Text('Hello'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
